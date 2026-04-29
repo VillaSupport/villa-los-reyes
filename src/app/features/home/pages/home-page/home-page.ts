ed7c36f1ce40bf979f9da3d959d8cadf886a7b79
davealba <david2001.004@gmail.com>
2026-04-28 22:19:25 -0400

    fixed imports

--- src/app/features/home/pages/home-page/home-page.ts
+++ src/app/features/home/pages/home-page/home-page.ts
@@ -6,10 +6,8 @@ import { CarouselSection } from '../../components/carousel-section/carousel-sect
 import { AboutView } from "../../../about/components/about-view/about-view";
 import { DiscoverRooms } from "../../../services-facilities/components/discover-room-section/discover-rooms";
 import { ReviewsCrossList } from "../../../reviews/components/reviews-cross-list/reviews-cross-list";
-import { ReviewsCrossList } from "../../../reviews/components/reviews-cross-list/reviews-cross-list";
 
 @Component({
-  imports: [ServiceCrossList, PackageCrossList, ExperiencesCrossSell, CarouselSection, AboutView, DiscoverRooms, ReviewsCrossList]
   imports: [ServiceCrossList, PackageCrossList, ExperiencesCrossSell, CarouselSection, AboutView, DiscoverRooms, ReviewsCrossList],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
